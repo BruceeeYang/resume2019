46f3921c568812a61b1b8a6e978a7b6a9942917f
Bruce Yang <46489186+BruceeeYang@users.noreply.github.com>
2019-02-02 12:30:02 +0800

    finish about me RWD position
    finish RWd aboutme and mobile scroll and appear aboutme-right-intro

--- all.js
+++ all.js
@@ -1,8 +1,40 @@
 $(document).ready(function () {
+    
+    
     $(".navbar-toggle").click(function(){
+
         $(".mobile-nav-bar-hide").slideToggle("slow");
+    
     });
+
+
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.aboutme-right-intro').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            // if( bottom_of_window > bottom_of_object ){
+                
+            //     $(this).animate({'opacity':'1'},500);
+                    
+            // }
+
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1000);
+                    
+            }
+            
+        }); 
+    
+    });
+
+
 });
-
-
 
