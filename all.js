f34634ad5d0daa0be27a27e0b54c43527d403e81
Bruce Yang <46489186+BruceeeYang@users.noreply.github.com>
2019-02-04 01:14:14 +0800

    finish experience RWD

--- all.js
+++ all.js
@@ -36,5 +36,33 @@ $(document).ready(function () {
     });
 
 
+
+
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('#timeline').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            // if( bottom_of_window > bottom_of_object ){
+                
+            //     $(this).animate({'opacity':'1'},500);
+                    
+            // }
+
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+    
+    });
+
+
 });
 
