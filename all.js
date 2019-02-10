a749755916b285ac74051e1858f01684e0d75622
Bruce Yang <46489186+BruceeeYang@users.noreply.github.com>
2019-02-10 16:21:03 +0800

    fix bookmark problem

--- all.js
+++ all.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
     
+    // var w = window.innerWidth;
+    // console.log(w);
+    // if (w>760) {
+    //     $('#home').css('height',w*0.5);
+    // } else if (w>480 && w<760) {
+    //     $('#home').css('height',w*0.12);
+    // } else {
+    //     $('#home').css('height',w*0.08);
+    // }
+
+
+    var s = window.innerHeight;
+    $('.scroll-down').css('bottom', s*0.18);
+    $('.mobile-scroll-dowm').css('top',s*0.78);
+
     
     $(".navbar-toggle").click(function(){
 
         $(".mobile-nav-bar-hide").slideToggle("slow");
     
     });
-
 
 
     /* Every time the window is scrolled ... */
@@ -19,12 +33,6 @@ $(document).ready(function () {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            // if( bottom_of_window > bottom_of_object ){
-                
-            //     $(this).animate({'opacity':'1'},500);
-                    
-            // }
-
             if( bottom_of_window > bottom_of_object ){
                 
                 $(this).animate({'opacity':'1'},800);
