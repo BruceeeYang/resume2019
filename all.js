af1e8b26b31a75167ad9e0b87afa5447fe31ce0c
Bruce Yang <46489186+BruceeeYang@users.noreply.github.com>
2019-02-12 19:37:46 +0800

    add RWD participation project slick slider

--- all.js
+++ all.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
     
-    var w = window.innerWidth;
-    console.log(w);
-    if (w>760) {
-        $('#home').css('height',w*0.5);
-    } else if (w>480 && w<760) {
-        $('#home').css('height',w*0.97);
-    } else {
-        $('#home').css('height',w*1.5);
-    }
+    // var w = window.innerWidth;
+    // console.log(w);
+    // if (w>760) {
+    //     $('#home').css('height',w*0.5);
+    // } else if (w>480 && w<760) {
+    //     $('#home').css('height',w*0.97);
+    // } else {
+    //     $('#home').css('height',w*1.5);
+    // }
+
+    $(window).resize(function() {
+        var w =$(window).width();
+        if (w>760) {
+            $('#home').css('height',w*0.5);
+        } else if (w>480 && w<760) {
+            $('#home').css('height',w*0.97);
+        } else {
+            $('#home').css('height',w*1.5);
+        }
+    });
 
 // ---------------------------------------------
 
@@ -31,6 +42,14 @@ $(document).ready(function () {
     
     });
 
+
+// -----------------------------------------------
+    $(".moblie-nav-bar-list-item a").click(function(){
+
+        $(".mobile-nav-bar-hide").slideUp("slow");
+    
+    });
+    
 // -----------------------------------------------
 
     /* Every time the window is scrolled ... */
@@ -86,16 +105,70 @@ $(document).ready(function () {
             var valorNivel = $(this).html("<span class='valor-nivel'>"+valorLargura+"</span>");
 
             if( bottom_of_window >= bottom_of_object ){   
-                $(this).animate({
-                    'width: valorLargura'
-                :'1'},300); 
-                // return;
-            }else{
                 $(this).css('width',valorLargura);
             }
+            
+
         }); 
     
     });
+
+
+    // --------------------------------------
+
+    $('.participationproject').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+
+
+    // ------------------------------------------
+    $('.companyname:nth-child(1)').hover(function () {
+            $('.companyrespensibility1').css('opacity','1')
+            
+        }, function () {
+            $('.companyrespensibility1').css('opacity','0')
+        }
+    );
+
+    $('.companyname:nth-child(2)').hover(function () {
+            $('.companyrespensibility2').css('opacity','1')
+            
+        }, function () {
+            $('.companyrespensibility2').css('opacity','0')
+        }
+    );
+
+    $('.companyname:nth-child(3)').hover(function () {
+            $('.companyrespensibility3').css('opacity','1')
+            
+        }, function () {
+            $('.companyrespensibility3').css('opacity','0')
+        }
+    );
+
+    $('.companyname:nth-child(4)').hover(function () {
+            $('.companyrespensibility4').css('opacity','1')
+            
+        }, function () {
+            $('.companyrespensibility4').css('opacity','0')
+        }
+    );
+
+    $('.companyname:nth-child(5)').hover(function () {
+            $('.companyrespensibility5').css('opacity','1')
+            
+        }, function () {
+            $('.companyrespensibility5').css('opacity','0')
+        }
+    );
+
+    // ------------------------------------------
+
+        
     
 
 
