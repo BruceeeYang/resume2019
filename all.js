320aa1a2e58fc355d0cf94db195c5b8eab826018
Bruce Yang <46489186+BruceeeYang@users.noreply.github.com>
2019-02-12 10:59:36 +0800

    fix #home size & position

--- all.js
+++ all.js
@@ -1,28 +1,37 @@
 $(document).ready(function () {
     
-    // var w = window.innerWidth;
-    // console.log(w);
-    // if (w>760) {
-    //     $('#home').css('height',w*0.5);
-    // } else if (w>480 && w<760) {
-    //     $('#home').css('height',w*0.12);
-    // } else {
-    //     $('#home').css('height',w*0.08);
-    // }
+    var w = window.innerWidth;
+    console.log(w);
+    if (w>760) {
+        $('#home').css('height',w*0.5);
+    } else if (w>480 && w<760) {
+        $('#home').css('height',w*0.12);
+    } else {
+        $('#home').css('height',w*1.8);
+    }
 
+// ---------------------------------------------
 
     var s = window.innerHeight;
-    $('.scroll-down').css('bottom', s*0.18);
-    $('.mobile-scroll-dowm').css('top',s*0.78);
+    var w = window.innerWidth;
+    if (w>760) {
+        $('.scroll-down').css('bottom', s*0.24);
+    } else if (w>480 && w<760) {
+        $('.scroll-down').css('bottom',s*0.12);
+    } else {
+        $('.mobile-scroll-dowm').css('bottom',s*0.2);
+    }
 
-    
+// ----------------------------------------------
+
+
     $(".navbar-toggle").click(function(){
 
         $(".mobile-nav-bar-hide").slideToggle("slow");
     
     });
 
-
+// -----------------------------------------------
 
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
@@ -44,7 +53,7 @@ $(document).ready(function () {
     
     });
 
-
+// -------------------------------------------------
 
     $(window).scroll( function(){
     
@@ -66,7 +75,7 @@ $(document).ready(function () {
     
     });
 
-
+// -----------------------------------------------------
     $(window).scroll( function(){
     
         $('.barra-nivel').each( function(i){
@@ -76,10 +85,10 @@ $(document).ready(function () {
             var valorLargura = $(this).data('nivel');
             var valorNivel = $(this).html("<span class='valor-nivel'>"+valorLargura+"</span>");
 
-            if( bottom_of_window > bottom_of_object ){   
+            if( bottom_of_window >= bottom_of_object ){   
                 $(this).animate({
                     width: valorLargura
-                },400);     
+                },300);     
             }
         }); 
     
